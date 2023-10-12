8ae57514808e710375d4b0c0dfc4190305b724ae
Luis Alexander Peñaloza Romero <luis.penaloza@wiedii.co>
2023-10-12 15:59:03 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const $d = document;
+
+$d.addEventListener("DOMContentLoaded", () => {
+  init();
+});
+
+function init() {
+  var demoWorkspace = Blockly.inject("blocklyDiv", {
+    media: "https://unpkg.com/blockly@10.1.3/media/",
+    toolbox: $d.getElementById("toolbox"),
+  });
+  Blockly.Xml.domToWorkspace($d.getElementById("startBlocks"), demoWorkspace);
+}
