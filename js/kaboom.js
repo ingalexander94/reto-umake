5c36f4dbf00601215bd155fa2cf9996ded53e078
Luis Alexander Peñaloza Romero <luis.penaloza@wiedii.co>
2023-10-30 17:01:53 -0500

    updating 30 octubre

--- js/kaboom.js
+++ js/kaboom.js
@@ -8,21 +8,35 @@ const frame = $d.querySelector("div.frame");
 
 const { offsetWidth: width, offsetHeight: height } = frame;
 
-const CONFIG = { width, height, canvas };
+const CONFIG = { width, height, canvas, global: true };
 
 const SPEED = 3000;
+
+const isMobile =
+  window.innerWidth < 1100 && screen.orientation.type === "landscape-primary";
 
 const createGame = () => {
   kaboom(CONFIG);
   loadAssets();
-  add([sprite("fondo"), layer("fondo"), pos(0, 0)]);
+  loadSprite("fondo", "../assets/img/fondo1.avif");
+  loadSprite(
+    "ship",
+    "https://cdn-umake.s3.us-east-2.amazonaws.com/wp-content/uploads/2023/08/16114324/nave-front.png"
+  );
+  add([sprite("fondo"), scale(1.1, 1.3)]);
   generateEnemies();
+  add([
+    timer(),
+    sprite("ship"),
+    pos(width - 80, height - 80),
+    area(),
+    scale(0.4, 0.4),
+  ]);
 };
 
 const loadAssets = () => {
   loadSprite("o1", "../assets/img/obstacles/1.webp");
   loadSprite("o2", "../assets/img/obstacles/2.webp");
-  loadSprite("fondo", "../assets/img/fondo.jpeg");
 };
 
 // fondo.scale = 2;
@@ -32,14 +46,14 @@ function generateEnemies() {
   if (enemies.length) {
     enemies.forEach((item) => item.destroy());
   }
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 6; i++) {
     const x = rand(0, width - 100);
     const y = rand(0, height - 100);
     const obstacle = generateNumberAleatory();
     const ghost = [
       sprite(`o${obstacle}`),
       pos(x, y),
-      scale(0.2, 0.2),
+      scale(isMobile ? 0.1 : 0.15, isMobile ? 0.1 : 0.15),
       area(),
       "enemy",
     ];
