bd0f5c9cf8433736581a0c1b976bba5d87c22e54
Luis Alexander Peñaloza Romero <luis.penaloza@wiedii.co>
2023-11-02 16:59:02 -0500

    cambios jueves 2 nov

--- js/kaboom.js
+++ js/kaboom.js
@@ -8,7 +8,7 @@ const frame = $d.querySelector("div.frame");
 
 const { offsetWidth: width, offsetHeight: height } = frame;
 
-const CONFIG = { width, height, canvas, global: true };
+const CONFIG = { width: 500, height: 500, canvas, global: true };
 
 const SPEED = 3000;
 
@@ -18,21 +18,23 @@ const isMobile =
 const createGame = () => {
   kaboom(CONFIG);
   loadAssets();
-  loadSprite("fondo", "../assets/img/fondo1.avif");
-  loadSprite("hearth", "../assets/img/hearth.png");
-  loadSprite(
-    "ship",
-    "https://cdn-umake.s3.us-east-2.amazonaws.com/wp-content/uploads/2023/08/16114324/nave-front.png"
-  );
-  add([sprite("fondo"), scale(1.1, 1.3)]);
+  add([sprite("layer"), scale(1, 1)]);
   generateEnemies();
-  add([sprite("ship"), pos(width - 80, height - 80), scale(0.4, 0.4)]);
-  add([sprite("hearth"), pos(250, 50), scale(0.03, 0.03)]);
+  add([sprite("player"), pos(500 - 50, 500 - 135), scale(0.03, 0.03)]);
+  add([sprite("bar1"), pos(500 - 110, 405), scale(0.06, 0.06)]);
+  add([sprite("bar2"), pos(405, 55), scale(0.06, 0.06)]);
 };
 
 const loadAssets = () => {
-  loadSprite("o1", "../assets/img/obstacles/1.webp");
-  loadSprite("o2", "../assets/img/obstacles/2.webp");
+  loadSprite("layer", "../assets/ui/layer.png");
+  loadSprite("player", "../assets/ui/player.png");
+  loadSprite("bar1", "../assets/ui/bar1.png");
+  loadSprite("bar2", "../assets/ui/bar2.png");
+  loadSprite("o1", "../assets/ui/obstacle1.png");
+  loadSprite("o2", "../assets/ui/obstacle2.png");
+  loadSprite("o3", "../assets/ui/obstacle3.png");
+  loadSprite("o4", "../assets/ui/obstacle4.png");
+  loadSprite("o5", "../assets/ui/obstacle5.png");
 };
 
 // fondo.scale = 2;
@@ -42,12 +44,11 @@ function generateEnemies() {
   if (enemies.length) {
     enemies.forEach((item) => item.destroy());
   }
-  for (let i = 0; i < 6; i++) {
-    const x = rand(0, width - 100);
-    const y = rand(0, height - 100);
-    const obstacle = generateNumberAleatory();
+  for (let i = 0; i < 5; i++) {
+    const x = rand(50, 300);
+    const y = rand(50, 450);
     const ghost = [
-      sprite(`o${obstacle}`),
+      sprite(`o${1 + i}`),
       pos(x, y),
       scale(isMobile ? 0.1 : 0.15, isMobile ? 0.1 : 0.15),
       area(),
